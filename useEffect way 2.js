2041915072e8ba194c10fd4bd089280336621276
cobaltune <cobaltune@naver.com>
2023-01-17 18:51:48 +0900

    useEffect

---
+++ useEffect way 2.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+
+function Hello() {
+  function byFn() {
+    console.log('bye');
+  }
+
+  function hiFn() {
+    console.log('created');
+    return byFn;
+  }
+  useEffect(function () {
+    console.log('hi');
+    return function () {
+      console.log('bye');
+    };
+  }, []);
+  return <h1>Hello</h1>;
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
+  return (
+    <div>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? 'Hide' : 'Show'}</button>
+    </div>
+  );
+}
+
+export default App;
